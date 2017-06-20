3149e2ad5dd1e8b66df9f5ba0ec7a2c4a8dee447
logonrm <logonrm@fiap.com.br>
2017-06-19 22:54:29 -0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express             =       require("express");
+var bodyParser          =       require("body-parser");
+var app                 =       express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var carros = [];
+
+app.get('/carros', function(request, response) {
+    response.send(carros);
+});
+
+app.post('/carros', function(request, response) {
+    carros.push(request.body);
+    response.send(201);
+});
+
+app.listen(3000, function() {
+    console.log('Servidor iniciado na porta 3000');
+});
